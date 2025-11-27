b4b9ff838f8e9fa482d979c89d4fb487d53e5d68
Atras22 <149415300+anonim57778@users.noreply.github.com>
2025-11-27 21:04:13 +0300

    test commit

--- src/app/(landing)/navbar.tsx
+++ src/app/(landing)/navbar.tsx
@@ -7,7 +7,7 @@ import { SheetClose } from "~/components/ui/sheet";
 export default function Navbar() {
     const menu = [
         {
-            name: "Десерты",
+            name: "Десертики",
             url: "/products/dessert"
         },
         {
